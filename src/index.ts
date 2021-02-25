296bdb2c4dec1edd5dd5360e4fd8e9d76b61abe2
原罪 <531362022@qq.com>
2021-02-25 18:33:43 +0800

    feat: Export SequelizeOptions

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export type { QueryInterface } from './types/override/QueryInterface';
 export { fn, literal, col, LOCK, Transaction } from 'sequelize';
 export { defineModel } from './model/defineModel';
 export { column } from './columns';
-export { Sequelize } from './model/Sequelize';
+export { Sequelize, SequelizeOptions } from './model/Sequelize';
 export { migration } from './migration/migration';
 export { seed } from './migration/seed';
 export { SequelizeSlot } from './slot/SequelizeSlot';
