3d7190c89165fd93c9ac0371910a1b644502091f
原罪 <531362022@qq.com>
2021-02-26 18:35:33 +0800

    feat: Export Op from original sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export type { Model } from './types/override/Model';
 export type { QueryInterface } from './types/override/QueryInterface';
-export { fn, literal, col, LOCK, Transaction } from 'sequelize';
+export { fn, literal, col, LOCK, Transaction, Op } from 'sequelize';
 export { defineModel } from './model/defineModel';
 export { column } from './columns';
 export { Sequelize, SequelizeOptions } from './model/Sequelize';
