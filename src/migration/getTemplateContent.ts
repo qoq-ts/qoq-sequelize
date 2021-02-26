7c4feeb4e6cc9eb7cf210f5b9f84598998ba4b13
原罪 <531362022@qq.com>
2021-02-26 17:01:56 +0800

    refactor: Rename templates to template

--- src/migration/getTemplateContent.ts
+++ src/migration/getTemplateContent.ts
@@ -2,5 +2,5 @@ import fs from 'fs';
 import path from 'path';
 
 export const getTemplateContent = (fileName: string) => {
-  return fs.readFileSync(path.join(__dirname, '..', 'templates', fileName + '.tpl')).toString();
+  return fs.readFileSync(path.join(__dirname, '..', 'template', fileName + '.tpl')).toString();
 };
