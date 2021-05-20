d7629deea63b6263ed2f9f32b40df08f67bccd37
原罪 <531362022@qq.com>
2021-05-20 09:29:39 +0800

    chore: Switch default module to ESM and test fallback to commonjs

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -2,18 +2,24 @@
 
 set -e
 
-rm -rf ./es/ ./lib/ ./types/
+rm -rf ./es/ ./lib/
+
+rm -rf ./build
+./node_modules/.bin/tsc  --module commonjs
+cp -R ./src/template ./build/src/template
+cp -R ./src/types ./build/src/types
+npx public-refactor --src ./src --dist ./build/src
+mv ./build/src ./lib
 
 rm -rf ./build
 ./node_modules/.bin/tsc
 cp -R ./src/template ./build/src/template
-mv ./build/src ./lib
-
-rm -rf ./build
-./node_modules/.bin/tsc --module es2020
-cp -R ./src/template ./build/src/template
+cp -R ./src/types ./build/src/types
+npx public-refactor --src ./src --dist ./build/src
 mv ./build/src ./es
 
-cp -R ./src/types ./build/types/src/types
-npx public-refactor --src ./src --dist ./build/types/src
-mv ./build/types/src ./types
+cat > ./es/package.json <<EOF
+{
+  "type": "module"
+}
+EOF
