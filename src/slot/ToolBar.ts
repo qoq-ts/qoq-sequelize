6ac2aca10b948f42214e22dbf16e5671f65f78fc
原罪 <531362022@qq.com>
2021-05-21 00:34:03 +0800

    fix: ESM doesn't support require

--- src/slot/ToolBar.ts
+++ src/slot/ToolBar.ts
@@ -4,15 +4,10 @@ import { Slot, version } from 'qoq';
 export class ToolBar extends Slot<Slot.Console> {
   constructor() {
     super();
-    const { version: sequelizeVersion } = require('sequelize/package.json');
     const nodeVersion = process.version.slice(1);
 
     this.use(async (_, next) => {
-      console.log(
-        chalk.underline(
-          `\nSequelize [Node: ${nodeVersion}, QoQ: ${version}, Sequelize: ${sequelizeVersion}]\n`,
-        ),
-      );
+      console.log(chalk.underline(`\nSequelize [Node: ${nodeVersion}, QoQ: ${version}]\n`));
 
       await next();
       console.log('');
